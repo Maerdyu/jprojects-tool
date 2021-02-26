780bccd2d36df17dd3c526c54523c1b67184a4ca
jinch <jinchunyu86@outlook.com>
2021-02-26 11:31:52 +0800

    init

---
+++ src/main/java/com/maerdyu/jprojectstool/JprojectsToolApplication.java
@@ -0,0 +1,13 @@
+package com.maerdyu.jprojectstool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JprojectsToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JprojectsToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maerdyu/jprojectstool/JprojectsToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maerdyu.jprojectstool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JprojectsToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
